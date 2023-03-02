fb31f09ebd3a3d3c5f33b60a07f2b93059c9a84d
junb119 <emfowkd@gmail.com>
2023-03-02 18:30:07 +0900

    test

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ window.addEventListener('load', () => {
 const $pw = document.querySelector('#pw');
 const $pwCheck = document.querySelector('#pw-check');
 const $submit = document.querySelector('#submit');
-const $modal = document.querySelector('#modal')
+const $modal = document.querySelector('#modal');
 
 // (공통) 모든 필드의 값은 빠짐 없이 입력해야 한다.
 // id : 5~20자, 영문 소문자, 숫자, 특수기호(_-)
@@ -41,25 +41,25 @@ const ErrorCheck = (whatInput, reg) => {
         // console.log(`${whatInput} blank`)
         ErrorMsg.innerText = '필수 정보입니다.';
         ValidCheck = false;
-        allVelidCheck[IdError] = false
+        allVelidCheck[IdError] = false;
     } else if (reg) {
         ValidCheck = reg.test(whatInput.value);
-        if(!ValidCheck) {
-          ErrorMsg.innerText = addErrorMsg[IdError]
-          allVelidCheck[IdError] = false
-        };
+        if (!ValidCheck) {
+            ErrorMsg.innerText = addErrorMsg[IdError];
+            allVelidCheck[IdError] = false;
+        }
     } else {
         ValidCheck = whatInput.value === $pw.value;
-        if(!ValidCheck) {
-          ErrorMsg.innerText = '비밀번호가 일치하지 않습니다.'
-          allVelidCheck[IdError] = false
-        };
+        if (!ValidCheck) {
+            ErrorMsg.innerText = '비밀번호가 일치하지 않습니다.';
+            allVelidCheck[IdError] = false;
+        }
     }
     // console.log('v', ValidCheck);
     if (ValidCheck) {
         whatInput.classList.remove('border-red-600');
         ErrorMsg.innerText = '';
-        allVelidCheck[IdError] = true
+        allVelidCheck[IdError] = true;
     } else {
         whatInput.classList.add('border-red-600');
     }
@@ -83,33 +83,75 @@ $id.addEventListener('focusout', () => idValidation($id));
 $pw.addEventListener('focusout', () => pwValidation($pw));
 $pwCheck.addEventListener('focusout', () => pwCheckValidation($pwCheck));
 
-const allVelidCheck ={
-  'id' : false,
-  'pw' : false,
-  'pw-check' : false
-}
+const allVelidCheck = {
+    id: false,
+    pw: false,
+    'pw-check': false,
+};
 
 $submit.addEventListener('click', (e) => {
-    e.preventDefault()
-    if (Object.values(allVelidCheck).indexOf(false)===-1) {
-      const confirmId = document.querySelector('#confirm-id')
-      const confirmPw = document.querySelector('#confirm-pw')
-      confirmId.innerText = $id.value
-      confirmPw.innerText = $pw.value
-      $modal.showModal()
+    e.preventDefault();
+    if (Object.values(allVelidCheck).indexOf(false) === -1) {
+        const confirmId = document.querySelector('#confirm-id');
+        const confirmPw = document.querySelector('#confirm-pw');
+        confirmId.innerText = $id.value;
+        confirmPw.innerText = $pw.value;
+        // $modal.open = true
+        $modal.showModal();
     }
 });
 
- const $cancelBtn = document.querySelector('#cancel-btn')
- const $approveBtn = document.querySelector('#approve-btn')
+const $cancelBtn = document.querySelector('#cancel-btn');
+const $approveBtn = document.querySelector('#approve-btn');
 
-$cancelBtn.addEventListener('click' , () => {
-  $modal.close()
-})
-$approveBtn.addEventListener('click' , () => {
-  alert('가입되었습니다.')
-  $modal.close()
-})
+$cancelBtn.addEventListener('click', () => {
+    $modal.close();
+});
+$approveBtn.addEventListener('click', () => {
+    alert('가입되었습니다.');
+    $modal.close();
+});
+
+// 'increase-font-btn'
+const $increaseFontBtn = document.querySelector('#increase-font-btn');
+const $decreaseFontBtn = document.querySelector('#decrease-font-btn');
+const $htmlEl = document.documentElement;
+const $currentHtmlFontsize = () =>
+    parseInt(window.getComputedStyle($htmlEl).fontSize);
+const maxFontSize = 20;
+const minFontSize = 12;
+
+$increaseFontBtn.addEventListener('click', () => {
+    fontControll('increase');
+});
+
+$decreaseFontBtn.addEventListener('click', () => {
+    fontControll('decrease');
+});
+
+const fontControll = (flg) => {
+  let currentFontsize = $currentHtmlFontsize()
+  let nextFontSize = flg === 'increase' ? currentFontsize + 1 :currentFontsize - 1
+  $increaseFontBtn.disabled = nextFontSize >= maxFontSize
+  $decreaseFontBtn.disabled = nextFontSize <= minFontSize
+  $htmlEl.style.fontSize = nextFontSize
+    // if (flg === 'increase') {
+    //     let nextFontSize = $currentHtmlFontsize() + 1;
+    //     if (nextFontSize >= maxFontSize) {
+    //         $increaseFontBtn.disabled = true;
+    //     }
+    //     if (nextFontSize > minFontSize) $decreaseFontBtn.disabled = false
+    //     $htmlEl.style.fontSize = nextFontSize;
+    // } else {
+    //     let nextFontSize = $currentHtmlFontsize() - 1;
+    //     if (nextFontSize <= minFontSize) {
+    //         $decreaseFontBtn.disabled = true;
+    //     }
+    //     if (nextFontSize < maxFontSize) $increaseFontBtn.disabled = false
+    //     $htmlEl.style.fontSize = nextFontSize;
+        
+    // }
+};
 
 // const checkIdValidation = (value) => {
 //   const ID_REGEX = new RegExp('^[a-z|0-9|-|_]{5,20}$')
